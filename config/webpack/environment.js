639fadc7cd93f7c173f70af79104e4927485028d
Brian Ildefonso <brianildefonso@Brians-MacBook-Pro.local>
2024-10-05 11:54:19 -0400

    first commit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,10 +6,10 @@ const customConfig = {
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+      '@utils': path.resolve(__dirname, '..', '..', 'app/javascript/src/utils'),
     }
   }
 }
-
 environment.config.merge(customConfig);
 
 environment.splitChunks()
